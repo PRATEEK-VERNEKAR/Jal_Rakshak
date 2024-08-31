b01b6b39cda7ef93061444646f798d60bdf57536
PRATEEK-VERNEKAR <mprateekvernekar189@gmail.com>
2024-08-31 13:23:09 +0530

    Search updated

--- src/app/components/Middle.jsx
+++ src/app/components/Middle.jsx
@@ -23,16 +23,32 @@ const Feed = () => {
 
   return (
     <div className="max-w-2xl mx-auto">
-      <div className="sticky top-0 bg-white z-10 mb-2">
-        <div className="relative">
-          <input
-            type="text"
-            placeholder="Search"
-            className=" bg-orange-100	 w-full h-[80px] pl-10 pr-4 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-300"
-          />
-          <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={18} />
-
-        </div>
+      <div className="sticky top-0 bg-white z-10 mb-2">   <div className="relative max-w-md mx-auto">
+      <input
+        type="text"
+        placeholder="Search"
+        className="w-full h-16 pl-14 pr-6 text-lg bg-gradient-to-r from-orange-50 to-orange-100 
+                   border-2 border-orange-200 rounded-full 
+                   focus:outline-none focus:ring-2 focus:ring-orange-300 focus:border-transparent
+                   transition-all duration-300 ease-in-out
+                   placeholder-orange-300 text-orange-800
+                   shadow-md hover:shadow-lg"
+      />
+      <Search 
+        className="absolute left-5 top-1/2 transform -translate-y-1/2 text-orange-400
+                   transition-all duration-300 ease-in-out
+                   group-hover:text-orange-600" 
+        size={24} 
+      />
+      <button 
+        className="absolute right-4 top-1/2 transform -translate-y-1/2
+                   bg-orange-500 text-white px-4 py-2 rounded-full
+                   hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-300
+                   transition-all duration-300 ease-in-out"
+      >
+        Search
+      </button>
+    </div>
       </div>
       
       {/* <div className="space-y-0" onClick={()=>{router.push('/posts?user=1')}}>
